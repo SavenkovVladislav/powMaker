f51aabd1c800c1376402949a04b84161655ee031
Vlad <savenkov.vla@yandex.ru>
2022-01-19 00:24:21 +0700

    script with comments

--- script.js
+++ script.js
@@ -4,37 +4,46 @@ const inputDegree = document.getElementById("inputDegree");
 const result = document.getElementById("result");
 
 function getResult() {
+    /*с помощью .value мы вытаскиваем значение, которое ввел пользователь в input и присваеваем эти значения переменным a и b*/
     let a = inputNumber.value;
     let b = inputDegree.value;
+    /*проверка на то, чтобы в поля были введены значения, иначе остановить функцию*/
     if (a == "" || b == "") {
         alert("Введите число и степень");
         return;
     }
 
-    let p = document.createElement('p');
-    p.classList.add('child');
-    p.innerHTML = getDegreeWhile(a, b);
-    result.appendChild(p);
+    let p = document.createElement('p'); //Создаёт новый элемент с заданным тегом
+
+    p.classList.add('child'); //Метод add объекта classList позволяет добавлять CSS классы элементу. Добавили класс child 
+
+    p.innerHTML = getDegree(a, b); //Свойство innerHTML позволяет получить и изменить текст элемента. В созданные параграфы записываем результат функции getDegree с ранее заданными a и b
+
+    result.appendChild(p); //Метод appendChild позволяет вставить в конец родительского элемента другой элемент. родитель.appendChild(элемент)
 }
 
 function getDegree(number, degree) {
     let result = number;
 
+    /*Любое число в степени 0 будет равно 1.*/
     if (degree == 0) {
         return 1;
     }
 
+    /*Метод Math.abs возвращает модуль числа*/
     for (i = Math.abs(degree); i !== 1; i--) {
         result = number * result;
     }
 
+    /*Проверяем на знак степени, если степень отрицательная, то вернуть 1/result*/
     if (degree > 0) {
         return result;
     }
     return 1 / result;
 
 }
-
+/*Если использовать цикл while*/
+/*
 function getDegreeWhile(number, degree) {
     let result = number;
 
@@ -45,3 +54,4 @@ function getDegreeWhile(number, degree) {
 
     return result
 }
+*/
